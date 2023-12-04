a8e93f2d1d32045d97ed6b2b690dc159fc2f31d1
Liispet-Tess Talu <liispet-tesstalu@LiispetTesssMBP.lan>
2023-12-04 22:06:30 +0200

    create new app

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rnd = new Random();
+
+int myRandomNum;
+
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    int MyRandomNum = rnd.Next(0, 11);
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {MyRandomNum}");
+}
+
+Console.WriteLine($"random sum total: {randomSum}");
+
+
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//FOR-LOOP
+//DRY-principle - Do Not Repeat Yourself
+
+int i = 0;
+
+for(int i = 0; i < 5; i++)  //i++ = i + 1
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
+
